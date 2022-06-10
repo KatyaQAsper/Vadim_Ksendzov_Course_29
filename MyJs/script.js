69db0c5547a56dfdf7539e81774cffe60ec8c950
Katya Qasper <linza97@mail.ru>
2022-06-10 15:30:58 +0400

    file update

--- MyJs/script.js
+++ MyJs/script.js
@@ -36,7 +36,7 @@ console.log(num3);
 
 /*let mum4 = 0;
 for (; num4 < 5; num4++) {
-    (num4 ==2) continue;
+   if (num4 ==2) continue;
     console.groupCollapsed(num4);
 }*/
 
@@ -48,3 +48,109 @@ for (; num5 < 5; num5++) {
 }
 console.log(`Работа окончена, num5 = ${num5}`);
 
+
+//Функции
+
+/*showMessage();
+ function showMessage() {
+     console.log("Сообщение");
+ }*/
+
+ let message; // объявили переменную
+
+ function showMessage(){
+     // используется внешняя переменная
+     message = "Сообщение";
+      }
+      showMessage();
+      console.log(message);
+
+
+     // Глобальные переменные удобны для данных, общих для всей программы
+
+      let globalVar = "Я глобальная переменнная";
+      function getSum() {
+          let numOne, numTwo;
+
+      function getNumOne() {
+          numOne = 1;
+          console.log(globalVar);
+
+      }   
+      function  getNumTwo() {
+      numTwo = 2;
+      }
+getNumOne();
+getNumTwo();
+
+let numSum = numOne + numTwo;
+console.log(globalVar);
+    }
+    console.log(globalVar);
+    getSum();
+
+
+    // Параметры (аргументы) функций
+
+    function calcSum(numOne, numTwo){
+        console.log('Переменная numOne:${numOne}');
+        console.log("Переменная numTwo:${numTwo}");
+        let numSum = numOne + numTwo;
+
+        console.log('Сумма:${numSum}');
+    }
+    calcSum(1, 2);
+
+
+    // setTimeout/ setInterval
+
+    function showNumber(num) {
+        console.log(num);
+        if (num < 5) {
+            setTimeout(showNumber, 1000, ++num);
+        }
+    }
+    setTimeout(showNumber, 1000, 1);
+
+
+    // отмена действия
+
+    function showNumber(num) {
+        console.log(num);
+        let timeId = setTimeout(showNumber, 1000, ++num)
+        if (num === 6) {
+            clearTimeout(timeId);
+        }
+    }
+    setTimeout(showNumber, 1000, 1);
+
+    
+    function showMessage(){
+        let message = "Всем привет! я учу JS";
+        
+        alert(message);
+    }
+    
+    
+
+    /*function showMessage() {
+        let message = "Привет! Я JavaScript!";
+        alert(message);
+    }*/
+
+    function checkAge(age) {
+        if (age > 18) {
+            return true;
+
+        } else {
+            return confirm("Вы хотите продолжить?");
+        }
+    }
+
+    let age = prompt('Сколько Вам лет?', 21);
+    if (checkAge(age)) {
+        alert("Доступ получен");
+
+    } else {
+        alert ("Доступ закрыт")
+    }
